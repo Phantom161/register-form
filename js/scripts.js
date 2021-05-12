3b10eda8db7adc6e98695cdf3be292ce5a048645
Maxim Nosal <nosal.max@gmail.com>
2021-05-12 16:51:07 +0300

    update

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$('.datepicker').Zebra_DatePicker({
+	format: 'd M Y',
+	months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+	days: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+	show_select_today: 'Сегодня',
+	lang_clear_date: 'Очистить',
+	default_position: 'above',
+	view: 'years',
+	direction: 0,
+});
